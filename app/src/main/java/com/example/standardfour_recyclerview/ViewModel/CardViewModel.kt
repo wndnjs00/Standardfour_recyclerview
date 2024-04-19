0b344e60087b6b6829acea3c485fbf30591e77a3
wndnjs00 <juwonjeon852@gmail.com>
2024-04-19 16:54:35 +0900

    [FEAT] ViewModel사용하여 MVVM모델로 변경

---
+++ app/src/main/java/com/example/standardfour_recyclerview/ViewModel/CardViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.standardfour_recyclerview.ViewModel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.standardfour_recyclerview.data.CardData
+import com.example.standardfour_recyclerview.data.DataSource
+
+class CardViewModel(dataSource: DataSource) : ViewModel() {
+
+    // data list observing을 사용하는 방법 (LiveData 사용안하고)
+    val cardData = dataSource.getCardList()
+
+    fun getCardModel(cardData: CardData) : CardData{
+        return cardData
+    }
+}
+
+
+class CardViewModelFactory : ViewModelProvider.Factory{
+
+    // 어떤 타입의 뷰모델이 와도 가능하게끔 (데이터타입도 어떤 데이터타입이든지 가능하도록)
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+
+        // modelClass(FlowerViewModel)가 유효한지 검사 (값이 있는지 체크)
+        // 즉, CardViewModel::class.java가 modelClass와 동일한 클래스인지를 검사
+        if (modelClass.isAssignableFrom(CardViewModel::class.java)){
+            // 값이 있다면, FlowerViewModel객체를 생성 / 이때, FlowerViewModel은 dataSource를 인자로 받아 초기화된다
+            // CardViewModel을 제네릭T타입으로 바꿔주겠다
+            return CardViewModel(dataSource = DataSource.getDataSoures()) as T
+        }
+
+        throw IllegalAccessException("Unknown ViewModel Class")
+    }
+}
--- app/src/main/java/com/example/standardfour_recyclerview/presentation/DetailActivity.kt
+++ app/src/main/java/com/example/standardfour_recyclerview/presentation/DetailActivity.kt
@@ -1,18 +1,16 @@
 package com.example.standardfour_recyclerview.presentation
 
 import android.os.Bundle
-import android.util.Log
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.os.bundleOf
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.standardfour_recyclerview.R
+import com.example.standardfour_recyclerview.ViewModel.CardViewModel
+import com.example.standardfour_recyclerview.ViewModel.CardViewModelFactory
 import com.example.standardfour_recyclerview.data.CardData
 import com.example.standardfour_recyclerview.data.DataSource
 import com.example.standardfour_recyclerview.databinding.ActivityDetailBinding
-import com.example.standardfour_recyclerview.databinding.ActivityMainBinding
+import com.example.standardfour_recyclerview.extension.extraNotNull
 import java.text.DecimalFormat
+
 
 class DetailActivity : AppCompatActivity() {
 
@@ -25,21 +23,52 @@ class DetailActivity : AppCompatActivity() {
         const val EXTRA_CARD : String = "extra_card"
     }
 
+    // 확장함수로 데이터받아오는거 구현한 함수 가져오기
+    private val card by extraNotNull<CardData>(EXTRA_CARD)
+
+
+    // ViewModel 선언
+    private val cardViewModel by viewModels<CardViewModel>{
+        CardViewModelFactory()
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        // Bundle을 사용해서 전달한 데이터 받아옴
-        val intent = getIntent()
+        // 1) Intent로 전달한 데이터 받아옴
+//        val intent = getIntent()
+//        val cardItem = intent?.getParcelableExtra<CardData>(EXTRA_CARD)
+//
+//        with(binding) {
+//            detailCardnameTv.text = "이름: ${cardItem?.name}"
+//            detailCardnunberTv.text = "카드번호: ${cardItem?.number}"
+//            detailDateTv.text = "유효기간: ${cardItem?.date}"
+//            detailPriceTv.text = "가격: $${DecimalFormat("#,##0.00").format(cardItem?.price)}"
+//        }
+
+
+        // 2) 확장함수로 넘긴 데이터 받아옴
+//        val cardData = card
+//
+//        with(binding) {
+//            detailCardnameTv.text = "이름: ${cardData.name}"
+//            detailCardnunberTv.text = "카드번호: ${cardData.number}"
+//            detailDateTv.text = "유효기간: ${cardData.date}"
+//            detailPriceTv.text = "가격: $${DecimalFormat("#,##0.00").format(cardData.price)}"
+//        }
+
+
+        // 3) viewModel 사용
         val cardItem = intent?.getParcelableExtra<CardData>(EXTRA_CARD)
+        val cardData = cardItem?.let { cardViewModel.getCardModel(it) }
 
         with(binding) {
-            detailCardnameTv.text = "이름: ${cardItem?.name}"
-            detailCardnunberTv.text = "카드번호: ${cardItem?.number}"
-            detailDateTv.text = "유효기간: ${cardItem?.date}"
-            detailPriceTv.text = "가격: $${DecimalFormat("#,##0.00").format(cardItem?.price)}"
+            detailCardnameTv.text = "이름: ${cardData?.name}"
+            detailCardnunberTv.text = "카드번호: ${cardData?.number}"
+            detailDateTv.text = "유효기간: ${cardData?.date}"
+            detailPriceTv.text = "가격: $${DecimalFormat("#,##0.00").format(cardData?.price)}"
         }
-
     }
 }
